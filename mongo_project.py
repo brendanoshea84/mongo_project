59e929bca1dcab11ad2e694ce904a8002d0a3697
brendanoshea84 <b_oshea_84@hotmail.com>
2020-03-11 19:05:01 +0100

    problems

---
+++ mongo_project.py
@@ -0,0 +1,68 @@
+import pymongo
+import os
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def show_menu():
+    print("")
+    print("1. Add a record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
+
+    option = input("Enter option: ")
+    return option
+
+def add_record():
+    first = input("Enter first name> ")
+    last = input("Enter first last> ")
+    dob = input("Enter date of birth> ")
+    gender = input("Enter gender> ")
+    occupation = input("Enter job> ")
+    nationality = input("Enter nationality> ")
+
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob, 'gender': gender, 'hair_colour': hair_colour, 'occupation': occupation, 'nationality': nationality}
+
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("doc inserted")
+    except:
+        print("ERROr")    
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == 1:
+            add_record()
+        elif option == 2:
+            print("You have selected option 2")
+        elif option == 3:
+            print("You have selected option 3")
+        elif option == 4:
+            print("You have selected option 4")
+        elif option == 5:
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+
+conn = mongo_connect(MONGODB_URI)
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+main_loop()
